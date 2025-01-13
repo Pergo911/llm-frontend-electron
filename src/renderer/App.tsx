2d43ddd699e8b843c31c450c7afc9f07e164dd8f
Pergo911 <probsztgeri@gmail.com>
2025-01-13 18:54:21 +0100

    Add shadcn/ui integration

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,11 +2,13 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.global.css';
 
 import 'tailwindcss/tailwind.css';
+import { Button } from './components/ui/button';
 
 function MainPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col gap-4 items-center justify-center h-screen">
       <div className="text-3xl font-bold">Hello world</div>
+      <Button>Click me</Button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,55 @@
-const colors = require('tailwindcss/colors');
+/** @type {import('tailwindcss').Config} */
+const animate = require('tailwindcss-animate');
 
 module.exports = {
   content: ['./src/renderer/**/*.{js,jsx,ts,tsx,ejs}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: ['class'], // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [animate],
 };
